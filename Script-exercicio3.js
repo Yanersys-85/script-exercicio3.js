31993125e82d341be13c5a561e525367638dd5ca
Yanersys-85 <105086754+Yanersys-85@users.noreply.github.com>
2022-06-26 16:16:10 -0300

    criando consulta ussando  promt em js

---
+++ Script-exercicio3.js
@@ -0,0 +1,11 @@
+//*exercicio3*//
+
+let nome = prompt ('qual é o seu nome?' , "yane");
+alert(`seu nome e  ${nome}!`);
+let sobrenome = prompt ('qual é o seu sobrenome?' , "jimenez");
+alert(`seu sobrenome e  ${sobrenome}!`);
+let edade = prompt ('qual é o sua adade?' , "43");
+alert(`sua edade e  ${edade}!`);
+
+
+
